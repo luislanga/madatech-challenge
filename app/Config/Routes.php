abf88438d835bed95081e2a604bb7391d58c3cd3
luislanga <luis.langajr96@gmail.com>
2025-03-06 11:58:45 -0300

    created rest api

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -19,3 +19,12 @@ $routes->post('/edit/(:num)', 'TaskController::editTask/$1');
 
 // Delete
 $routes->get('/delete/(:num)', 'TaskController::delete/$1');
+
+// API
+$routes->group('api', function($routes) {
+    $routes->get('tasks', 'Api\TaskController::index');
+    $routes->get('task/(:num)', 'Api\TaskController::show/$1');
+    $routes->post('task', 'Api\TaskController::create');
+    $routes->put('task/(:num)', 'Api\TaskController::update/$1');
+    $routes->delete('task/(:num)', 'Api\TaskController::delete/$1');
+});
